e464c5532cf68810b131ae607ac2402fda6ad2d1
robert_gevorgyan <gevorgyanrk@gmail.com>
2021-09-09 11:01:38 +0300

    Initial commit

---
+++ src/main/java/com/gevorgyan/javafour/JavaFourApplication.java
@@ -0,0 +1,13 @@
+package com.gevorgyan.javafour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFourApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gevorgyan/javafour/JavaFourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gevorgyan.javafour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFourApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
